cec709b7ad4d831673e8166501a517cd3e486eca
bradmarcloud <77006721+Bayley-Bradley@users.noreply.github.com>
2022-12-19 15:12:19 +0000

    Changed name of reactive variable

--- views/Scan.js
+++ views/Scan.js
@@ -1,6 +1,13 @@
 import { View, Text } from "react-native";
 
+import { useReactiveVar } from "@apollo/client";
+import { userDetailsVar, accessTokenVar } from "../views/Login";
+
 export default function Scan({ navigation }) {
+  const USER_DETAILS = useReactiveVar(userDetailsVar);
+
+  console.log(item);
+
   return (
     <View>
       <Text>Welcome to the scan screen</Text>
