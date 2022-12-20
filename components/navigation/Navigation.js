8b2e9ee5c91aa35db8d403e1e3f576bfc14abddf
bradmarcloud <77006721+Bayley-Bradley@users.noreply.github.com>
2022-12-20 16:31:58 +0000

    Added new screen

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../../views/Login";
 import Scan from "../../views/Scan";
+import ProspectInformation from "../../views/ProspectInformation";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ function Navigation() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Scan" component={Scan} />
+        <Stack.Screen name="ProspectInformation" component={ProspectInformation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
