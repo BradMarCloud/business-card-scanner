722d30c8febd0d04d6463e95d392092c73d3f984
BradMarCloud <brad@marcloudconsulting.com>
2022-12-20 11:24:41 +0000

    UseEffect still loggedin

--- views/Scan.js
+++ views/Scan.js
@@ -1,10 +1,29 @@
 import { View, Text } from "react-native";
-
 import { useReactiveVar } from "@apollo/client";
 import { userDetailsVar, accessTokenVar } from "../views/Login";
+import { useEffect } from "react";
+import { getToken, loggedIn } from "../components/graphql/util/auth";
 
 export default function Scan({ navigation }) {
+  const isFocused = navigation.isFocused();
+
   const USER_DETAILS = useReactiveVar(userDetailsVar);
+
+  useEffect(() => {
+    async function stillLoggedIn() {
+      let token = await getToken();
+      console.log("Token = " + token);
+
+      const isLoggedIn = await loggedIn();
+      if (isLoggedIn) {
+        console.log("Login Expiry");
+        navigation.replace("Login");
+        await auth.logout();
+      }
+      console.log("Still Logged In");
+    }
+    stillLoggedIn();
+  }, [isFocused]);
 
   // console.log(USER_DETAILS);
 
