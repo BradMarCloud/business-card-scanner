1f8893a24b1b66030d06ee4182bd862da6b50bca
BradMarCloud <brad@marcloudconsulting.com>
2022-12-20 11:23:27 +0000

    Fixed async functions

--- components/graphql/util/auth.js
+++ components/graphql/util/auth.js
@@ -1,32 +1,47 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import jwt_Decode from "jwt-decode";
 
-export const auth = {
-  async getProfile() {
-    const token = await this.getToken();
-    let decodedToken = jwt_Decode(token);
-    return decodedToken;
-  },
-  async loggedIn() {
-    const token = await this.getToken();
-    const tokenExpired = await this.isTokenExpired(token);
-    return token && !tokenExpired ? true : false;
-  },
-  async isTokenExpired(token) {
-    const decodedToken = jwt_Decode(token);
-    if (decodedToken.exp > Date.now() / 1000) {
+export async function getProfile() {
+  const token = await getToken();
+  let decodedToken = jwt_Decode(token);
+  return decodedToken;
+}
+export async function loggedIn() {
+  const token = await getToken();
+  const tokenExpired = await isTokenExpired(token);
+  return token && tokenExpired ? true : false;
+}
+export async function isTokenExpired(token) {
+  const decodedToken = jwt_Decode(token);
+  if (decodedToken.exp <= Date.now() / 1000) {
+    try {
       await AsyncStorage.removeItem("id_token");
       return true;
+    } catch (error) {
+      console.error(error);
     }
-    return false;
-  },
-  async getToken() {
-    return await AsyncStorage.getItem("id_token");
-  },
-  async login(token) {
-    return await AsyncStorage.setItem("id_token", token);
-  },
-  async logout() {
-    return await AsyncStorage.removeItem("id_token");
-  },
-};
+  }
+  return false;
+}
+export async function getToken() {
+  try {
+    const token = await AsyncStorage.getItem("id_token");
+    return token;
+  } catch (error) {
+    console.error(error);
+  }
+}
+export async function loggingIn(token) {
+  try {
+    await AsyncStorage.setItem("id_token", token);
+  } catch (error) {
+    console.error(error);
+  }
+}
+export async function logout() {
+  try {
+    await AsyncStorage.removeItem("id_token");
+  } catch (error) {
+    console.error(error);
+  }
+}
