7402c7733e5895a9d85f40075ce542fdce32f8da
bradmarcloud <77006721+Bayley-Bradley@users.noreply.github.com>
2022-12-20 16:32:42 +0000

    Scan image function connected

--- views/Scan.js
+++ views/Scan.js
@@ -3,6 +3,7 @@ import { useReactiveVar } from "@apollo/client";
 import { userDetailsVar, accessTokenVar } from "../views/Login";
 import { useEffect } from "react";
 import { getToken, loggedIn } from "../components/graphql/util/auth";
+import ScanImage from "../components/ui/scanImage/ScanImage";
 
 export default function Scan({ navigation }) {
   const isFocused = navigation.isFocused();
@@ -30,6 +31,7 @@ export default function Scan({ navigation }) {
   return (
     <View>
       <Text>Welcome to the scan screen</Text>
+      <ScanImage />
     </View>
   );
 }
